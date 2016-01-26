a44985e032019389ebcd721b8bf0038f7a311416
niklasberglund <niklas@asdasd.net>
2016-01-26 17:46:38 +0800

    properly HeaderDoc formatting

--- Categories/NSObject+description.h
+++ Categories/NSObject+description.h
@@ -8,18 +8,18 @@
 
 @interface NSObject (description)
 
-/**
+/*!
  * Returns a NSString formatted in the same way as Apple classes' description methods, with data from the members specified in the memberVariables argument.
  *
- * @param memberVariables Dictionary with member variables and their data. Example: @{@"title": self.title, @"text":self.text}
- * @returns Formatted string with the data of members in the memberVariables dictionary.
+ * @param memberVariables Dictionary with member variables and their data. Example: \@{\@"title": self.title, \@"text":self.text}
+ * @return Formatted string with the data of members in the memberVariables dictionary.
  */
 - (NSString *)descriptionWithMembers:(NSDictionary *)memberVariables;
 
-/**
+/*!
  * Returns a NSString formatted in the same way as Apple classes' description methods, with data from all properties of this object.
  *
- * @returns Formatted string with the data of all properties of this object.
+ * @return Formatted string with the data of all properties of this object.
  */
 - (NSString *)descriptionWithAllProperties;
 
