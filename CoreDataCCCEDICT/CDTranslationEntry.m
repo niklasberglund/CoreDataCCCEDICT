07d6cc7bcc7d0ca0e8f4b0fcd1dc7e24809880f4
niklasberglund <niklas@asdasd.net>
2016-01-27 23:44:21 +0800

    implemented methods returning NSManagedObject instances

--- CoreDataCCCEDICT/CDTranslationEntry.m
+++ CoreDataCCCEDICT/CDTranslationEntry.m
@@ -40,4 +40,41 @@
                                           }];
 }
 
+#pragma mark - NSManagedObject creation
+- (NSManagedObject *)chineseManagedObjecInManagedObjectContext:(NSManagedObjectContext *)managedObjectContext
+{
+    NSManagedObject *chineseManagedObject = [NSEntityDescription insertNewObjectForEntityForName:@"Chinese_entry" inManagedObjectContext:managedObjectContext];
+    [chineseManagedObject setValue:self.simplifiedChinese forKey:@"simplified"];
+    [chineseManagedObject setValue:self.traditionalChinese forKey:@"traditional"];
+    [chineseManagedObject setValue:self.pinyin forKey:@"pinyin"];
+    
+    return chineseManagedObject;
+}
+
+- (NSArray <NSManagedObject *>*)englishManagedObjectsInManagedObjectContext:(NSManagedObjectContext *)managedObjectContext
+{
+    NSMutableArray <NSManagedObject *>*englishTranslations = [NSMutableArray new];
+    
+    for (NSString *english in self.englishTranslations) {
+        NSManagedObject *englishManagedObject = [NSEntityDescription insertNewObjectForEntityForName:@"English_entry" inManagedObjectContext:managedObjectContext];
+        [englishManagedObject setValue:english forKey:@"english"];
+        
+        [englishTranslations addObject:englishManagedObject];
+    }
+    
+    return englishTranslations;
+}
+
+- (NSManagedObject *)entryManagedObjectWithChinese:(NSManagedObject *)chineseManagedObject english:(NSArray<NSManagedObject *> *)englishManagedObjects date:(NSDate *)databaseDate inManagedObjectContext:(NSManagedObjectContext *)managedObjectContext
+{
+    NSManagedObject *entryManagedObject = [NSEntityDescription insertNewObjectForEntityForName:@"Entry" inManagedObjectContext:managedObjectContext];
+    //[entryManagedObject setValue:[NSDate date] forKey:@"added"];
+    [entryManagedObject setValue:databaseDate forKey:@"modified"];
+    
+    [entryManagedObject setValue:[NSSet setWithArray:englishManagedObjects] forKey:@"inEnglish"];
+    [entryManagedObject setValue:chineseManagedObject forKey:@"inChinese"];
+    
+    return entryManagedObject;
+}
+
 @end
