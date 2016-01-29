29a3837eeeb1d043f1d61c608fafc4346d9e6652
niklasberglund <niklas@asdasd.net>
2016-01-29 18:38:58 +0800

    minor semantic improvement in NSObject+description documentation

--- Categories/NSObject+description.h
+++ Categories/NSObject+description.h
@@ -9,7 +9,7 @@
 @interface NSObject (description)
 
 /*!
- * Returns a NSString formatted in the same way as Apple classes' description methods, with data from the members specified in the memberVariables argument.
+ * Creates a description string formatted in the same way as Apple classes' description methods', with data from the members specified in the memberVariables argument.
  *
  * @param memberVariables Dictionary with member variables and their data. Example: \@{\@"title": self.title, \@"text":self.text}
  * @return Formatted string with the data of members in the memberVariables dictionary.
@@ -17,7 +17,7 @@
 - (NSString *)descriptionWithMembers:(NSDictionary *)memberVariables;
 
 /*!
- * Returns a NSString formatted in the same way as Apple classes' description methods, with data from all properties of this object.
+ * Creates a description string formatted in the same way as Apple classes' description methods', with data from all properties of this object.
  *
  * @return Formatted string with the data of all properties of this object.
  */
