d19bb7323b75af87a7925841b2efef3b9ca5ec5a
niklasberglund <niklas@asdasd.net>
2016-01-30 15:20:57 +0800

    moved NSSet conversion out of method call

--- CoreDataCCCEDICT/CDTranslationEntry.m
+++ CoreDataCCCEDICT/CDTranslationEntry.m
@@ -68,10 +68,12 @@
 - (NSManagedObject *)entryManagedObjectWithChinese:(NSManagedObject *)chineseManagedObject english:(NSArray<NSManagedObject *> *)englishManagedObjects date:(NSDate *)databaseDate inManagedObjectContext:(NSManagedObjectContext *)managedObjectContext
 {
     NSManagedObject *entryManagedObject = [NSEntityDescription insertNewObjectForEntityForName:@"Entry" inManagedObjectContext:managedObjectContext];
+    NSSet *englishManagedObjectsSet = [NSSet setWithArray:englishManagedObjects];
+    
     //[entryManagedObject setValue:[NSDate date] forKey:@"added"];
     [entryManagedObject setValue:databaseDate forKey:@"modified"];
     
-    [entryManagedObject setValue:[NSSet setWithArray:englishManagedObjects] forKey:@"inEnglish"];
+    [entryManagedObject setValue:englishManagedObjectsSet forKey:@"inEnglish"];
     [entryManagedObject setValue:chineseManagedObject forKey:@"inChinese"];
     
     return entryManagedObject;
